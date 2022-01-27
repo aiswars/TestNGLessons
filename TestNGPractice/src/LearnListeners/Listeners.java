eb08449b673917cfae83c765eedae8eefdcdf216
aisdhar <75646664+aisdhar@users.noreply.github.com>
2022-01-27 14:33:47 -0500

    ReportsNote.txt,screenshot links,thumbnails to emailablereport.html, Reporter.log,

--- TestNGPractice/src/LearnListeners/Listeners.java
+++ TestNGPractice/src/LearnListeners/Listeners.java
@@ -3,6 +3,7 @@ package LearnListeners;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.Reporter;
 
 public class Listeners implements ITestListener{
 	
@@ -24,8 +25,25 @@ public class Listeners implements ITestListener{
 
 		System.setProperty("org.uncommons.reportng.escape-output","false");
 		 //Reporter.log("<a href=\"F:\\screenshot\\error.jpg\" target=\"_blank\">Screenshot link</a>");
+		
 		//Reporter.log("<br>");
-	//	Reporter.log("<a href=\"F:\\screenshot\\error.jpg\" target=\"_blank\"><img height=200 width=200 src=\"F:\\screenshot\\error.jpg\"></a>");
+		//Reporter.log("<a href=\"F:\\screenshot\\error.jpg\" target=\"_blank\"><img height=200 width=200 src=\"F:\\screenshot\\error.jpg\"></a>");
+	
+		Reporter.log("Reporter.log: Failed test>> Screenshot link");//to print in emailablereport.html
+		Reporter.log("Reporter.log  Screenshot link "); //to print in emailablereport.html
+	
+		//to add a link to screenshot location C:\\automation\\screenshots\\sample.png
+		Reporter.log("  <a href=\"C:\\automation\\screenshots\\sample.png\">   Screenshotlink opening same window </a>"); //screenshot link in emailablereport.html
+	
+		//link to open in new wiwndow>>   target=\"_blank\" 
+		Reporter.log("  <a href=\"C:\\automation\\screenshots\\sample.png\" target=\"_blank\">   Screenshotlink opening in new window </a>"); //opening screenshot in new window
+		
+		Reporter.log("<br>");//break- to get a newline
+		//adding a thumbnail, instead of text.
+		Reporter.log("  <a href=\"C:\\automation\\screenshots\\sample.png\" target=\"_blank\">  <img height=200 width=200 src=\"C:\\automation\\screenshots\\sample.png\">    </a>");
+	 
+		//Reporter.log("  <a href=\"F:\\screenshot\\error.jpg\"               target=\"_blank\"> <img height=200 width=200 src=\"F:\\screenshot\\error.jpg\"> </a>");
+		
 		System.out.println("-------FAILED TEST---------"); 
 		System.out.println("-------------Capturing Screenshot for the Failed test-------------- ");
 		System.out.println(" The method failed is    "+result.getName());
